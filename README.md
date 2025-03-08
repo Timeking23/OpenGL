5aa5822655b65513cfad1031919e37901d35f0cd
Timeking23 <danielgetahun46@gmail.com>
2025-03-08 05:04:44 -0500

    relearned the program

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Compile command:
     python compiler.py
     main.py
     
+#arch linux pre-install
+# Update system and install packages  
+sudo pacman -Syu  
+  
+# Install C++ development tools, Clang, and make  
+sudo pacman -S clang make g++  
+  
+# Install OpenGL, GLFW, and GLEW  
+sudo pacman -S mesa glfw glew  
+  
+# Install X11 libraries (needed for GLFW and OpenGL)  
+sudo pacman -S libx11 libxrandr libxi libxinerama libxcursor  
+  
+# Install static library creation tool  
+sudo pacman -S binutils  
+  
--- main.cpp
+++ main.cpp
@@ -1,43 +1,43 @@
-#include <glad/gl.h>
+#include "dependencies/include/glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <cstdlib>
 #include <iostream>
 
 #define WIDTH 1920 
 #define HEIGHT 1080
+
+void display(GLFWwindow* window, double currentTime){
+    glClearColor(0.5,0.3,0.4,1);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 void error_callback(int error, const char* description)
 {
     std::cerr << "error code (" << error <<") description: " << description << "\n";
 }
-
 int main(){
     glfwSetErrorCallback(error_callback);
-
     if (!glfwInit())
-    {
-        // Initialization failed
-    }
+    {   exit(EXIT_FAILURE);    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "My Title", NULL, NULL);
-    if (!window)
-    {
-        std::cout << "window failed to create\n";
-        // Window or OpenGL context creation failed
-    }
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Tutorial", NULL, NULL);
     glfwMakeContextCurrent(window);
     gladLoadGL();
+    glfwSwapInterval(1);
 
     while (!glfwWindowShouldClose(window))
     {
+        
         glViewport(0, 0, WIDTH, HEIGHT);
-        // Keep running
+        display(window,glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 
     }
 
 
     glfwDestroyWindow(window);
     glfwTerminate();
-
-    
-
+    exit(EXIT_SUCCESS);
 }
